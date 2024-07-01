452ecf50b5598b02067a0510c8408d24411b78bf
robynainsley21 <robynainsley2i@gmail.com>
2024-07-01 08:37:02 +0200

    Create side comps

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="text-center fixed-bottom">
+        <div class="row">
+            <p class="lead mt-4">
+                Robyn's Portfolio &copy; <span>{{ cYear }}</span>
+            </p>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "FooterComp",
+    data() {
+        return {
+            cYear: new Date().getUTCFullYear(),
+        };
+    },
+};
+</script>
+
+<style></style>
---
+++ src/components/Spinner.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SpinnerComp",
+};
+</script>
+
+<style></style>

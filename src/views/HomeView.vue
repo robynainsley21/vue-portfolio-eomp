f703bf30ebb68e9bfcf9899f01ef761b06ee1d35
robynainsley21 <robynainsley2i@gmail.com>
2024-07-01 08:27:15 +0200

    Add content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="row" id="landing">
+      <LandingSection />
+    </section>
+    <section class="row" id="about">
+      <h2 class="p-4 text-center">About</h2>
+      <div class="row">
+        <AboutSection />
+      </div>
+    </section>
+    <section class="row" id="resume">
+      <h2 class="p-4 text-center">Resume</h2>
+      <ResumeSection />
+    </section>
+    <section class="row" id="skills">
+      <h2 class="p-4 text-center">Skills</h2>
+      <SkillsSection />
+    </section>
+    <section class="row" id="testimonials">
+      <h2 class="p-4 text-center">Testimonials</h2>
+      <TestimonialSection />
+    </section>
+    <section class="row text-center" id="projects">
+      <h2 class="p-4">Projects</h2>
+      <ProjectsSection />
+    </section>
+    <section class="row max-height" id="contact">
+      <h2 class="p-4">Contact</h2>
+      <ContactSection />
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LandingSection from '@/components/LandingSection.vue'
+import AboutSection from '@/components/AboutSection.vue'
+import ResumeSection from '@/components/ResumeSection.vue';
+import SkillsSection from '@/components/SkillsSection.vue'
+import TestimonialSection from '@/components/TestimonialSection.vue'
+import ProjectsSection from '@/components/ProjectsSection.vue'
+import ContactSection from '@/components/ContactSection.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    LandingSection,
+    AboutSection,
+    ResumeSection,
+    SkillsSection,
+    TestimonialSection,
+    ProjectsSection,
+    ContactSection
   }
 }
 </script>

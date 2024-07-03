79391765354765539a8988f9e0ddf8c93d97a35a
robynainsley21 <robynainsley2i@gmail.com>
2024-07-03 10:57:23 +0200

    Modify skills section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,36 +1,36 @@
 <template>
-    <div class="row" id="about-content">
-        <div class="col-md-6">
-            <img src="" alt="about-img">
-        </div>
-        <div v-if="about?.length" class="col-md-6 about-text">
-            <p>{{ about[0]?.text1 }}</p>
-            <p>{{ about[0]?.text2 }}</p>
-            <ul v-if="about?.length" class="wrapper">
-           <a :href="about.github" target="_blank" class="icon github">
-                <span class="tooltip">Github</span>
-                <span><i class="fab fa-github"></i></span>
-            </a>
-            <a :href="about.linkedin" target="_blank" class="icon linkedin">
-                <span class="tooltip">LinkedIn</span>
-                <span><i class="fab fa-linkedin"></i></span>
-            </a>
-        </ul>
-        </div>
-        <SpinnerComp v-else />
+  <div class="row" id="about-content">
+    <div class="col-md-6">
+      <img src="" alt="about-img" />
     </div>
+    <div v-if="about?.length" class="col-md-6 about-text">
+      <p>{{ about[0]?.text1 }}</p>
+      <p>{{ about[0]?.text2 }}</p>
+      <ul v-if="about?.length" class="wrapper d-flex justify-content-center">
+        <a :href="about.github" target="_blank" class="icon github">
+          <span class="tooltip">Github</span>
+          <span><i class="fab fa-github"></i></span>
+        </a>
+        <a :href="about.linkedin" target="_blank" class="icon linkedin">
+          <span class="tooltip">LinkedIn</span>
+          <span><i class="fab fa-linkedin"></i></span>
+        </a>
+      </ul>
+    </div>
+    <SpinnerComp v-else />
+  </div>
 </template>
 
 <script setup>
-import SpinnerComp from '@/components/Spinner.vue'
-import { computed, onMounted } from 'vue'
-import { useStore } from 'vuex'
-const store = useStore()
-const about = computed(() => store.state.about)
+import SpinnerComp from "@/components/Spinner.vue";
+import { computed, onMounted } from "vue";
+import { useStore } from "vuex";
+const store = useStore();
+const about = computed(() => store.state.about);
 
 onMounted(() => {
-    store.dispatch('fetchAbout')
-})
+  store.dispatch("fetchAbout");
+});
 </script>
 
 <style></style>
--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -1,16 +1,15 @@
 <template>
     <div v-if="projects?.length" class="row gap-4">
         <div class="card p-0 project-card" v-for="project in projects" :key="project.id">
-            <img class="card-image" :src="project.img_url" alt="project-img" />
+            <img class="card-image" :src="project.img_url" alt="project-img" loading='lazy'/>
             <div class="m-auto text-center project-header">
                 <h5>{{ project.projectName }}</h5>
                 <p>{{ project.description }}</p>
                 <div class="buttons-box">
-                    <a class="btn" type="button" :href="project.github" target="blank">GitHub</a>
-                    <a class="btn" type="button" :href="project.hosted_link" target="blank">Live site</a>
+                    <a class="btn project-btn" type="button" :href="project.github" target="blank">GitHub</a>
+                    <a class="btn project-btn" type="button" :href="project.hosted_link" target="blank">Live site</a>
                 </div>
             </div>
-
         </div>
     </div>
     <SpinnerComp v-else />
